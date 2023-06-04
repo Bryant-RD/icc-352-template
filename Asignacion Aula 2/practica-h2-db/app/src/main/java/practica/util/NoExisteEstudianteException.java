0d287129d133a2d8b3382e4968d8262d44acb560
Bryant-RD <briant04.rt@gmail.com>
2023-06-04 13:35:19 -0400

    Subiendo asignacion en clase 2 integrando JDBC con H2 a crudSimple

---
+++ Asignacion Aula 2/practica-h2-db/app/src/main/java/practica/util/NoExisteEstudianteException.java
@@ -0,0 +1,8 @@
+package practica.util;
+
+public class NoExisteEstudianteException extends RuntimeException {
+
+    public NoExisteEstudianteException(String message) {
+        super(message);
+    }
+}
